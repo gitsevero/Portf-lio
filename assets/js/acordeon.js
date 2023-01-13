e2cba7bb293eaf67c96ce644384cb0225f9e4ee0
severo <119140931+gitsevero@users.noreply.github.com>
2023-01-13 03:48:35 -0300

    go

---
+++ assets/js/acordeon.js
@@ -0,0 +1,1 @@
+const acordeonTriggers=document.querySelectorAll(".acordeon .trigger");acordeonTriggers.forEach(n=>{n.addEventListener("click",e=>{const o=n.parentElement;o.classList.contains("open")?o.classList.remove("open"):o.classList.add("open")})});
