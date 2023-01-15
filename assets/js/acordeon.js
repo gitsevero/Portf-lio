c7898fae03e26fb70406b6fad81400076554f288
gitsevero <thierrymuller2015@gmail.com>
2023-01-15 14:39:31 -0300

    FINAL

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,1 +1,7 @@
-const acordeonTriggers=document.querySelectorAll(".acordeon .trigger");acordeonTriggers.forEach(n=>{n.addEventListener("click",e=>{const o=n.parentElement;o.classList.contains("open")?o.classList.remove("open"):o.classList.add("open")})});
+const acordeonTriggers = document.querySelectorAll(".acordeon .trigger");
+acordeonTriggers.forEach(n => {
+    n.addEventListener("click", e => {
+        const o = n.parentElement;
+        o.classList.contains("open") ? o.classList.remove("open") : o.classList.add("open")
+    })
+});
